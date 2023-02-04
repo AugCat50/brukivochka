b7bb2268c0395b1680bf846924f5815638d7d5a3
AugCat50 <augcat50@gmail.com>
2023-02-04 08:34:15 +0200

    First raw template added

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
+    Route::get('/', 'IndexController')->name('main.index');
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function() {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController')->name('main.index');
+    });
 });
 
 Auth::routes();
