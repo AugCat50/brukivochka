2f184ee6afc17505ac16bb4808a1b51bfe9e320f
AugCat50 <augcat50@gmail.com>
2023-02-05 06:11:29 +0200

    Categories CRUD added and migration softDel

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -32,7 +32,7 @@
             </div>    
 
             <div class="row">
-                <div class="col-4">
+                <div class="col-5">
                     <div class="card">
                         <div class="card-body table-responsive p-0">
                             <table class="table table-hover text-nowrap">
@@ -52,6 +52,16 @@
                                         <td>{{ $category->title }}</td>
                                         <td>{{ $category->created_at }}</td>
                                         <td><a href="{{ route('admin.category.show', $category->id) }}"><i class="far fa-eye"></i></a></td>
+                                        <td><a class="text-success" href="{{ route('admin.category.edit', $category->id) }}"><i class="fas fa-pencil-alt"></i></a></td>
+                                        <td>
+                                            <form action="{{ route('admin.category.delete', $category->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="border-0 bg-transparent">
+                                                    <i class="fas fa-trash text-danger" role="button"></i>
+                                                </button>
+                                            </form>
+                                        </td>
                                     </tr>
                                     @endforeach
                                 </tbody>
--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,13 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::get('/', 'IndexController')->name('main.index');
     });
     Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
-        Route::get ('/'      , 'IndexController')  ->name('admin.category.index');
-        Route::get ('/create', 'CreateController') ->name('admin.category.create');
-        Route::post('/'      , 'StoreController')  ->name('admin.category.store');
-        Route::get('/{category}', 'ShowController')->name('admin.category.show');
+        Route::get   ('/'                 , 'IndexController') ->name('admin.category.index');
+        Route::get   ('/create'           , 'CreateController')->name('admin.category.create');
+        Route::post  ('/'                 , 'StoreController') ->name('admin.category.store');
+        Route::get   ('/{category}'       , 'ShowController')  ->name('admin.category.show');
+        Route::get   ('/{category}/edit'  , 'EditController')  ->name('admin.category.edit');
+        Route::patch ('/{category}/update', 'UpdateController')->name('admin.category.update');
+        Route::delete('/{category}/delete', 'DeleteController')->name('admin.category.delete');
     });
 });
 
